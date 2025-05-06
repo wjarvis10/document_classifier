3e9fc88385b7dad7c8ffeea30dfccd0f90e42ffa
Will Jarvis <william_jarvis@brown.edu>
2025-05-06 09:44:32 -0400

    added cors

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from src.classifier import classify_file
 import logging
 
 app = Flask(__name__)
+CORS(app, resources={r"/classify_file": {"origins": "http://localhost:5173"}})
 
 # Global logging config
 logging.basicConfig(
